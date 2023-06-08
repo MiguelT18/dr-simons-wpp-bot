220094b6ac374ff46636f74bc652ed1aa39fdaa2
Miguel Teran <miguel.teranj02@gmail.com>
2023-06-08 17:49:30 -0400

    agregué la lógica de cuando el usuario envía su comprobante de pago, falta hacer que funcione

--- base-baileys-json/chatgpt.class.js
+++ base-baileys-json/chatgpt.class.js
@@ -1,4 +1,4 @@
-const { CoreClass } = require("@bot-whatsapp/bot");
+const { CoreClass, MessageType } = require("@bot-whatsapp/bot");
 const keyPoints = require("./src/keyPoints.json");
 const emoji = require("node-emoji");
 const emojiRegex = require("emoji-regex");
@@ -8,13 +8,15 @@ class chatGPTClass extends CoreClass {
   optionGPT = { model: "gpt-3.5-turbo" };
   openai = undefined;
   keyPoints = keyPoints;
+  awaitingPaymentConfirmation = false;
+  awaitingReceipt = false;
 
   constructor(_database, _provider, _optionsGPT = {}) {
     super(null, _database, _provider);
     this.init().then();
   }
 
-  // Iniciando...
+  //? Iniciando...
   init = async () => {
     const { ChatGPTAPI } = await import("chatgpt");
     this.openai = new ChatGPTAPI({
@@ -22,8 +24,60 @@ class chatGPTClass extends CoreClass {
     });
   };
 
+  //? Manejo de Mensajes
   handleMsg = async (ctx) => {
-    const { from, body } = ctx;
+    const { from, body, type, hasMedia } = ctx;
+
+    if (this.awaitingPaymentConfirmation) {
+      //! El bot está esperando la confirmación de pago
+
+      if (body.toLowerCase() === "comprobante") {
+        // El usuario ha enviado un mensaje con la palabra "comprobante"
+        // Solicitar el comprobante al usuario
+
+        this.awaitingPaymentConfirmation = false;
+        this.awaitingReceipt = true;
+        console.log("Se solicita el comprobante de pago");
+        this.sendFlowSimple(
+          [
+            {
+              type: MessageType.text,
+              body: "Por favor, envía el comprobante de pago.",
+            },
+          ],
+          from
+        );
+        return;
+      }
+
+      // Si el usuario no envía un mensaje con la palabra "comprobante", continúa con la interaciión normal
+      this.awaitingPaymentConfirmation = false;
+    }
+
+    if (this.awaitingReceipt) {
+      //! El bot está esperando el comprobante de pago
+
+      if (type === MessageType.image && hasMedia) {
+        // El usuario ha enviado una imagen como comprobante de pago
+
+        this.awaitingReceipt = false;
+        console.log("Se recibió un comprobante de pago");
+        this.sendFlowSimple(
+          [
+            {
+              type: MessageType.text,
+              body: "Gracias por enviar tu comprobante de pago. Procesaré tu solicitud.",
+            },
+          ],
+          from
+        );
+
+        return;
+      }
+
+      // Si el usuario no envía una imagen como comprobante, continuar con la interacción normal
+      this.awaitingReceipt = false;
+    }
 
     // Verificar si es el primer mensaje de la conversación
     const isFirstMessage = this.queue.length === 0;
@@ -61,10 +115,8 @@ class chatGPTClass extends CoreClass {
     this.sendFlowSimple([parseMessage], from);
   };
 
+  //? BUILDPROMPT
   buildPrompt = (userMessage, isFirstMessage) => {
-    const greeting =
-      "¡Hola! Como asistente AI al servicio del Dr.Cristian Simons, tu misión es manejar consultas sobre programación de citas, pagos, procedimientos quirúrgicos y dudas en general en el contexto de la consulta médica en línea.";
-
     let formattedKeyPoints = "";
     if (Array.isArray(this.keyPoints)) {
       formattedKeyPoints = this.keyPoints
@@ -72,11 +124,14 @@ class chatGPTClass extends CoreClass {
         .join("\n");
     }
 
-    const prompt = `
-      ${greeting}
-      ${formattedKeyPoints}
-      ${userMessage}
-    `;
+    let prompt = userMessage;
+    if (isFirstMessage) {
+      const greeting =
+        "¡Hola! Como asistente AI al servicio del Dr.Cristian Simons, tu misión es manejar consultas sobre programación de citas, pagos, procedimientos quirúrgicos y dudas en general en el contexto de la consulta médica en línea.";
+      prompt = `${greeting}\n${formattedKeyPoints}\n${userMessage}`;
+    } else {
+      prompt = `${formattedKeyPoints}\n${userMessage}`;
+    }
 
     return prompt.trim();
   };
